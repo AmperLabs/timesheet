631c77b2965b05de19eb12ce6c3d1c1038f4bb8f
neumannalex <40916845+neumannalex@users.noreply.github.com>
2025-01-27 21:33:29 +0100

    Improved data structure for weekly bookings

--- Timesheet/Data/TimesheetWeek.cs
+++ Timesheet/Data/TimesheetWeek.cs
@@ -9,12 +9,47 @@ namespace Timesheet.Data
         public int Year { get; init; }
         public int WeekOfYear { get; init; }
 
-        public List<TimesheetDay> Days { get; set; }
+        public List<TimesheetDay> BookedDays { get; set; }
 
-        public TimeSpan TotalWorkingTimeInPresence => TimeSpan.FromSeconds(Days.Where(x => x.WorkingTimeInPresence.HasValue).Select(x => x.WorkingTimeInPresence!.Value.TotalSeconds).Sum());
-        public TimeSpan TotalMobileWork => TimeSpan.FromSeconds(Days.Where(x => x.MobileWork.HasValue).Select(x => x.MobileWork!.Value.TotalSeconds).Sum());
-        public TimeSpan TotalWorkingTime => TimeSpan.FromSeconds(Days.Where(x => x.TotalWorkingTime.HasValue).Select(x => x.TotalWorkingTime!.Value.TotalSeconds).Sum());
-        public TimeSpan OvertimeHours => TimeSpan.FromSeconds(Days.Where(x => x.OvertimeHours.HasValue).Select(x => x.OvertimeHours!.Value.TotalSeconds).Sum());
+        private TimeSpan? _bookedMobileWork = null;
+        public TimeSpan? BookedMobileWork
+        {
+            get => _bookedMobileWork;
+            set
+            {
+                if(_bookedMobileWork != value)
+                {
+                    _bookedMobileWork = value;
+                    IsDirty = true;
+                }
+            }
+        }
+        
+        private DateOnly? _bookedForDate = null;
+        public DateOnly? BookedForDate
+        {
+            get => _bookedForDate;
+            set
+            {
+                if (_bookedForDate != value)
+                {
+                    _bookedForDate = value;
+                    IsDirty = true;
+                }
+            }
+        }
+
+        public bool IsDirty { get; private set; }
+        public void ResetDirtyFlag()
+        {
+            IsDirty = false;
+        }
+
+        public TimeSpan TotalWorkingTimeInPresence => TimeSpan.FromSeconds(BookedDays.Where(x => x.WorkingTimeInPresence.HasValue).Select(x => x.WorkingTimeInPresence!.Value.TotalSeconds).Sum());
+        public TimeSpan TotalMobileWork => TimeSpan.FromSeconds(BookedDays.Where(x => x.MobileWork.HasValue).Select(x => x.MobileWork!.Value.TotalSeconds).Sum());
+        public TimeSpan TotalWorkingTime => TimeSpan.FromSeconds(BookedDays.Where(x => x.TotalWorkingTime.HasValue).Select(x => x.TotalWorkingTime!.Value.TotalSeconds).Sum());
+        public TimeSpan OvertimeHours => TimeSpan.FromSeconds(BookedDays.Where(x => x.OvertimeHours.HasValue).Select(x => x.OvertimeHours!.Value.TotalSeconds).Sum());
+        public TimeSpan UnbookedMobileWork => BookedMobileWork.HasValue ? TotalMobileWork - BookedMobileWork.Value : TotalMobileWork;
 
         public double? MobileWorkShare => TotalWorkingTime.TotalSeconds > 0 ? TotalMobileWork.TotalSeconds / TotalWorkingTime.TotalSeconds : null;
         public double? PresenceWorkShare => TotalWorkingTime.TotalSeconds > 0 ? TotalWorkingTimeInPresence.TotalSeconds / TotalWorkingTime.TotalSeconds : null;
--- Timesheet/Entities/WeekRecord.cs
+++ Timesheet/Entities/WeekRecord.cs
@@ -1,5 +1,7 @@
 ﻿using MongoDB.Driver;
 using System.Globalization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using Timesheet.Common;
 
 namespace Timesheet.Entities
 {
@@ -7,20 +9,22 @@ namespace Timesheet.Entities
     {
         public string Id { get; set; }
         public string UserId { get; set; }
+        public TimeSpan? BookedMobileWork { get; set; }
+        public DateOnly? BookedForDate { get; set; }
 
-        public static string GenerateKey(DateTime date)
+        public static string GenerateKey(DateTime date, string userId)
         {
-            return GenerateKey(date.Year, ISOWeek.GetWeekOfYear(date));
+            return GenerateKey(date.Year, ISOWeek.GetWeekOfYear(date), userId);
         }
-
-        public static string GenerateKey(DateOnly date)
+        public static string GenerateKey(DateOnly date, string userId)
         {
-            return GenerateKey(date.Year, ISOWeek.GetWeekOfYear(date.ToDateTime(new TimeOnly(0, 0, 0))));
+            return GenerateKey(date.Year, ISOWeek.GetWeekOfYear(date.ToDateTime(new TimeOnly(0, 0, 0))), userId);
         }
-
-        public static string GenerateKey(int year, int weekNumber)
+        public static string GenerateKey(int year, int weekNumber, string userId)
         {
-            return $"{year}{weekNumber:D2}";
+            var normalizedUserId = userId.NormalizeEmail();
+
+            return $"{normalizedUserId}-{year}{weekNumber:D2}";
         }
     }
 }
